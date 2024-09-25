8dbd00944026432a215222a64ecf0aa71b571118
G00414989@atu.ie <eddiewilkinson000@gmail.com>
2024-09-25 14:56:16 +0100

    Added method Greet by name

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -1,9 +1,9 @@
 package ie.atu.week2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping("/person")
 public class RequestController {
@@ -11,5 +11,10 @@ public class RequestController {
     public String sayHello()
     {
         return "Hi there :)";
+    }
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name)
+    {
+        return "Your Name is " + name;
     }
 }
